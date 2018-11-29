cc687d476549155120c345bd21cd9d37d416dc0e
iggy12345 <32805004+iggy12345@users.noreply.github.com>
2018-11-29 17:47:50 -0600

    Created an expense wizard for creating expenses

---
+++ BudgettingTool/BudgettingTools/Templates/ExpenseWizard.Designer.cs
@@ -0,0 +1,86 @@
+﻿namespace BudgettingTools.Templates
+{
+    partial class ExpenseWizard
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.aExpenseForm1 = new BudgettingTools.Templates.FormsControls.AExpenseForm();
+            this.buttonAccept = new System.Windows.Forms.Button();
+            this.buttonCancel = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // aExpenseForm1
+            // 
+            this.aExpenseForm1.Location = new System.Drawing.Point(12, 12);
+            this.aExpenseForm1.Name = "aExpenseForm1";
+            this.aExpenseForm1.Size = new System.Drawing.Size(515, 451);
+            this.aExpenseForm1.TabIndex = 0;
+            this.aExpenseForm1.ValueChangedEvent += new BudgettingTools.Templates.FormsControls.AExpenseForm.ValueChangedEventHandler(this.aExpenseForm1_ValueChangedEvent);
+            // 
+            // buttonAccept
+            // 
+            this.buttonAccept.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Right)));
+            this.buttonAccept.Location = new System.Drawing.Point(512, 469);
+            this.buttonAccept.Name = "buttonAccept";
+            this.buttonAccept.Size = new System.Drawing.Size(75, 23);
+            this.buttonAccept.TabIndex = 1;
+            this.buttonAccept.Text = "Accept";
+            this.buttonAccept.UseVisualStyleBackColor = true;
+            this.buttonAccept.Click += new System.EventHandler(this.buttonAccept_Click);
+            // 
+            // buttonCancel
+            // 
+            this.buttonCancel.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Right)));
+            this.buttonCancel.Location = new System.Drawing.Point(431, 469);
+            this.buttonCancel.Name = "buttonCancel";
+            this.buttonCancel.Size = new System.Drawing.Size(75, 23);
+            this.buttonCancel.TabIndex = 2;
+            this.buttonCancel.Text = "Cancel";
+            this.buttonCancel.UseVisualStyleBackColor = true;
+            this.buttonCancel.Click += new System.EventHandler(this.buttonCancel_Click);
+            // 
+            // ExpenseWizard
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(599, 506);
+            this.Controls.Add(this.buttonCancel);
+            this.Controls.Add(this.buttonAccept);
+            this.Controls.Add(this.aExpenseForm1);
+            this.Name = "ExpenseWizard";
+            this.Text = "ExpenseWizard";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private FormsControls.AExpenseForm aExpenseForm1;
+        private System.Windows.Forms.Button buttonAccept;
+        private System.Windows.Forms.Button buttonCancel;
+    }
+}
---
+++ BudgettingTool/BudgettingTools/Templates/ExpenseWizard.cs
@@ -0,0 +1,100 @@
+﻿using BudgettingTools.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace BudgettingTools.Templates
+{
+    public partial class ExpenseWizard : Form
+    {
+        #region Properties
+
+        /// <summary>
+        /// Contains the Expense that this wizard was tasked to create.
+        /// </summary>
+        public AExpense Value { get; protected set; } = new AExpense();
+
+        #endregion
+
+        #region Events
+
+        public delegate void ValueChangedEventHandler(object sender, EventArgs e);
+        /// <summary>
+        /// Triggered every time that the value property changes.
+        /// </summary>
+        public event ValueChangedEventHandler ValueChangedEvent;
+        protected void OnValueChangedEvent()
+        {
+            ValueChangedEvent?.Invoke(this, new EventArgs());
+        }
+
+        public delegate void AcceptedEventHandler(object sender, EventArgs e);
+        /// <summary>
+        /// Triggered every time that the value property changes.
+        /// </summary>
+        public event AcceptedEventHandler AcceptedEvent;
+        protected void OnAcceptedEvent()
+        {
+            AcceptedEvent?.Invoke(this, new EventArgs());
+        }
+
+        public delegate void NewAccountEventHandler(object sender, EventArgs e);
+        /// <summary>
+        /// Triggered every time that a new account is created using the account linking wizard
+        /// </summary>
+        public event AcceptedEventHandler NewAccountEvent;
+        protected void OnNewAccountEvent(IAccount acct)
+        {
+            AcceptedEvent?.Invoke(acct, new EventArgs());
+        }
+
+        #endregion
+
+        #region Constructors
+
+        public ExpenseWizard()
+        {
+            InitializeComponent();
+        }
+
+        public ExpenseWizard(ICollection<IAccount> globalAccountsList)
+        {
+            aExpenseForm1 = new FormsControls.AExpenseForm(globalAccountsList);
+            aExpenseForm1.NewAccountEvent += AExpenseForm1_NewAccountEvent;
+            InitializeComponent();
+        }
+
+        #endregion
+
+        #region Methods
+
+        private void AExpenseForm1_NewAccountEvent(object sender, EventArgs e)
+        {
+            OnNewAccountEvent((IAccount)sender);
+        }
+
+        private void buttonAccept_Click(object sender, EventArgs e)
+        {
+            OnAcceptedEvent();
+            Dispose();
+        }
+
+        private void buttonCancel_Click(object sender, EventArgs e)
+        {
+            Dispose();
+        }
+
+        private void aExpenseForm1_ValueChangedEvent(object sender, EventArgs e)
+        {
+            OnValueChangedEvent();
+        }
+
+        #endregion
+    }
+}
