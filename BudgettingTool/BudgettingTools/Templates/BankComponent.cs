6141dc362922982553b2ef714d893cca56783f53
iggy12345 <32805004+iggy12345@users.noreply.github.com>
2018-11-29 17:47:29 -0600

    Added regions and some other things
    Added a creation date property, a method for assigning a custom id to a component, and overrode the .Equals() method.

--- BudgettingTool/BudgettingTools/Templates/BankComponent.cs
+++ BudgettingTool/BudgettingTools/Templates/BankComponent.cs
@@ -12,6 +12,8 @@ namespace BudgettingTools.Templates
     /// </summary>
     public class BankComponent
     {
+        #region Properties
+
         /// <summary>
         /// The title used to describe this component
         /// </summary>
@@ -21,6 +23,8 @@ namespace BudgettingTools.Templates
         /// The description used to give this component context
         /// </summary>
         public string Description { get; set; } = "";
+
+        public DateTime CreationDate { get; private set; } = DateTime.Now;
 
         /// <summary>
         /// A unique identifier for this object, it is the ComponentCount++
@@ -32,6 +36,20 @@ namespace BudgettingTools.Templates
         /// </summary>
         public static int ComponentCount { get; private set; } = 0;
 
+        #endregion
+
+        #region Methods
+
+        /// <summary>
+        /// Assigns a new ID to this bank component
+        /// USE AT YOUR OWN RISK
+        /// </summary>
+        /// <param name="newID">The new integer ID to assign to this component</param>
+        public void AssignID(int newID)
+        {
+            ID = newID;
+        }
+
         /// <summary>
         /// Converts the class into a string
         /// </summary>
@@ -40,5 +58,28 @@ namespace BudgettingTools.Templates
         {
             return Title + ID;
         }
+
+        /// <summary>
+        /// Compares this bank component to an object
+        /// If the object is an int, it checks the int against its own id
+        /// If the object is a string it checks the string against its own title
+        /// If the object is a bank component it compares the two objects IDs'
+        /// If the object is something else it calls the base object .Equals function
+        /// </summary>
+        /// <param name="obj">Object to compare this component to</param>
+        /// <returns>Returns true if the two objects are considered equal, returns false otherwise.</returns>
+        public override bool Equals(object obj)
+        {
+            if (obj.GetType().Equals(((int)0).GetType()))
+                return (int)obj == ID;
+            else if (obj.GetType().Equals("".GetType()))
+                return ((string)obj) == Title;
+            else if (obj.GetType() != GetType())
+                return base.Equals(obj);
+            else
+                return ID == ((BankComponent)obj).ID;
+        }
+
+        #endregion
     }
 }
