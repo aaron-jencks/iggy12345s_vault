25d0f02143b24203bad1ad48e3b88faab8e9533c
iggy12345 <32805004+iggy12345@users.noreply.github.com>
2018-11-30 15:55:14 -0600

    Update AFirework.cs
    Implemented basic methods and properties

--- Fireworks/FireworkToolkit/Templates/AFirework.cs
+++ Fireworks/FireworkToolkit/Templates/AFirework.cs
@@ -7,9 +7,60 @@ using System.Threading.Tasks;
 
 namespace FireworkToolkit.Templates
 {
-    public abstract class AFirework : IFirework
+    public abstract class AFirework : AParticle, IFirework
     {
-        public abstract bool Done();
-        public abstract void Explode(int qty = 100);
+        #region Properties
+
+        /// <summary>
+        /// A collection of particles currently in use by the firework
+        /// </summary>
+        protected ICollection<IParticle> particles { get; private set; } = new List<IParticle>();
+
+        /// <summary>
+        /// A flag specifying if the firework has exploded yet, or not
+        /// </summary>
+        protected bool Exploded { get; set; } = false;
+
+        /// <summary>
+        /// The current alpha of the particles
+        /// </summary>
+        protected int ExplosionAlpha { get; set; } = 255;
+
+        /// <summary>
+        /// Flags whether this object is currently using it's collection, or drawing, etc...
+        /// It's doing stuff
+        /// </summary>
+        public bool Busy { get; protected set; } = false;
+
+        /// <summary>
+        /// The magnitude of force that occurs when this firework explodes
+        /// </summary>
+        public int ExplosionMag { get; set; } = 10;
+
+        /// <summary>
+        /// How far from the center of the explosion that the particles are placed when drawing
+        /// </summary>
+        public int ExplosionPlacementRadius { get; set; } = 5;
+
+        /// <summary>
+        /// How quickly the particles diminish per step
+        /// </summary>
+        public int ParticleDiminishRate { get; set; } = 4;
+
+        #endregion
+
+        #region Methods
+
+        public virtual bool Done()
+        {
+            return (Exploded && particles.Count == 0);
+        }
+
+        public virtual void Explode(int qty = 100)
+        {
+            Exploded = true;
+        }
+
+        #endregion
     }
 }
