b3c2957a21420b7077773aaf11367508e2a7b2d3
iggy12345 <32805004+iggy12345@users.noreply.github.com>
2018-11-30 18:19:16 -0600

    Create PhysicsLib.cs
    Used to perform physics calculations on vectors

---
+++ Fireworks/FireworkToolkit/PhysicsLib.cs
@@ -0,0 +1,20 @@
+﻿using FireworkToolkit.Templates;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FireworkToolkit
+{
+    public static class PhysicsLib
+    {
+        public static void Collision(ref AParticle a, ref AParticle b, int time = 1)
+        {
+            AVector bu = (AVector)b.Velocity.Clone();
+
+            b.Velocity = 2 * a.Mass * a.Velocity / (a.Mass + b.Mass) + (b.Mass - a.Mass) / (a.Mass + b.Mass) * b.Velocity;
+            a.Velocity = 2 * b.Mass * bu / (a.Mass + b.Mass) - (b.Mass - a.Mass) / (a.Mass + b.Mass) * a.Velocity;
+        }
+    }
+}
